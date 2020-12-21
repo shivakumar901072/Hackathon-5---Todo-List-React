24ac4dbf6a41e7db2296a2db7bb9739df751bc11
shivakumar901072 <shivakumar901072@gmail.com>
2020-12-21 22:20:12 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
+import ListItem from "./ListItem";
+function App() {
+	const [items,setItems]=useState([]);
+	const [newItem, setNewItem]=useState("");
+	const addItem=()=>{
+		items.push(newItem);
+		setItems([...items]);
 
-function App() 
-{
+		setNewItem("");
+	};
+	const newItemChanged=(evt)=>{
+		setNewItem(evt.target.value);
+	}
+	const deleteHandler=(itemIdx)=>{
+		items.splice(itemIdx,1);
+		setItems([...items]);
+	}
+	const editHandler=(editedValue, itemIdx)=>{
+		items[itemIdx]=editedValue;
+		setItems([...items]);
+	};
+
 	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
+	<div id="main">	
+		<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}></textarea>
+		<button id="btn" onClick={addItem} disabled={newItem.trim().length===0}>Add Item</button>
+		{
+			items.map((item,idx)=>(
+				<ListItem item={item} editHandler={editHandler} 
+				key={`${item}_${idx}`} idx={idx} deleteHandler={deleteHandler}/>
+			))
+		}
 	</div>
 	);
 }
-
+ 
 
 export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./../styles/App.css";
+
+function ListItem(props) 
+{
+	const [editedItem,setEditedItem]=useState(props.item);
+	const [editMode,setEditMode]=useState(false);
+	const editedItemChanged=(evt)=>{
+		setEditedItem(evt.target.value);
+	}
+	const saveEditedItem=()=>{
+		props.editHandler(editedItem,props.idx);
+		setEditMode(false);
+	}
+	return (
+	<div className="list">	
+		{editMode ? (
+		<>
+			<textarea className="editTask" onChange={editedItemChanged} placeholder="Edit task" value={editedItem}></textarea>
+			<button className="saveTask" onClick={saveEditedItem} disabled={editedItem.trim().length===0}>Save</button>
+		</>
+		): (
+		<>
+			{props.item}
+			<button className="edit" onClick={()=>setEditMode(true)}>Edit</button>
+			<button className="delete" onClick={()=>props.deleteHandler(props.idx)}>Delete</button>
+		</>
+		)}		
+	</div>
+	);
+}
+
+
+export default ListItem;
